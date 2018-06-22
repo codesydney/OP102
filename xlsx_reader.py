38261305ab6008fdb429b2e1b0e9f79aaa94c950
Engramar Bollas <engramar.bollas@gmail.com>
2018-06-22 18:15:36 +1000

    Add files via upload

---
+++ xlsx_reader.py
@@ -0,0 +1,15 @@
+# Import pandas
+import pandas as pd
+
+# Convert excel to pandas dataframe
+file = 'sample_sales.xlsx'
+xl = pd.ExcelFile(file)
+df = xl.parse('Sheet1')
+
+# Read each line from pandas dataframe
+for index, row in df.iterrows():
+	    account_number = row[0]
+	    account_name   = row[1]
+	    sku            = row[2]
+	    category       = row[3]
+	    print (account_number,account_name,sku,category)
